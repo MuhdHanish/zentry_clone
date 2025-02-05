31897f50d79ccad490c0743066c2646ce83930b6
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-05 21:26:21 +0530

    style: extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,23 @@ export default {
         "robert-regular": ["robert-regular", "sans-serif"],
         "robert-medium": ["robert-medium", "sans-serif"],
         "circluar-web": ["circluar-web", "sans-serif"],
-      }
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
     },
   },
   plugins: [],
