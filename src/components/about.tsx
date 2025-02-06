a287be6f27dd2f8eda3ddabdbe39b36548f7d4fb
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-06 09:28:23 +0530

    feat: created about component

---
+++ src/components/about.tsx
@@ -0,0 +1,53 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const About = () => {
+    useGSAP(() => { 
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        });
+
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0,
+        });
+    });
+
+    return (
+        <div id="about" className="min-h-screen w-screen">
+            <div className="relatvie mb-8 mt-36 flex flex-col items-center gap-5">
+                <h2 className="font-general text-sm uppercase md:text-[10px]">
+                    Welcom to Zentry
+                </h2>
+                <h1 className="hero-heading mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem] special-font">
+                    Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure
+                </h1>
+                <div className="about-subtext">
+                    <p>The Metagame begins-your life, now an epic MMORPG</p>
+                    <p>Zentry is unified play layer that bridges players, agentic AI, and blockchains, creating a new conomic paradigm.</p>
+                </div>
+            </div>
+            <div id="clip" className="h-dvh w-screen">
+                <div className="mask-clip-path about-image">
+                    <img
+                        src="./img/about.webp"
+                        alt="about-background-image"
+                        title="About Background Image"
+                        className="absolute left-0 top-0 size-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
