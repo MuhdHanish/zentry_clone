0ac5d4656634051015998b6eb5071a48c21bdd4a
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-06 21:22:43 +0530

    style: addded inset x 3 in small device

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -51,7 +51,7 @@ export const NavBar = () => {
     }, [isAudioOn]);
 
     return (
-        <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
+        <div ref={navContainerRef} className="fixed top-4 z-50 h-16 border-none transition-all duration-700 inset-x-3 sm:inset-x-6">
             <header className="absolute top-1/2 w-full -translate-y-1/2">
                 <nav className="flex size-full items-center justify-between p-4">
                     <div className="flex items-center gap-7">
