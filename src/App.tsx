e9726befb8c7a85ae332bd9587c05e245f3a6535
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-06 07:24:01 +0530

    feat: added hero section in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import {
+  Hero
+} from "./components"
+
 export const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-teal-500 font-black">
-        Zentry Clone
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   )
 }
