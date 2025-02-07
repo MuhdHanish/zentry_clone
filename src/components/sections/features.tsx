ce36f0d160220d5d69cb907e2ae00b03d8331b45
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-07 08:09:10 +0530

    refactor: added mix blend diffrence to bento card text

--- src/components/sections/features.tsx
+++ src/components/sections/features.tsx
@@ -42,7 +42,7 @@ const BentoCard = ({
                 autoPlay={autoPlay}
                 className="absolute left-0 top-0 size-full object-cover object-center"
             />
-            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50 mix-blend-difference">
                 <div>
                     <h3 className="bento-title special-font">{title}</h3>
                     {description && (
